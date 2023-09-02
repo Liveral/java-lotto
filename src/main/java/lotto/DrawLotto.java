78ed5c9c0ec9464741de0bcb69ceffaa96132866
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-02 18:04:28 +0900

    feat : 당첨 번호를 입력하는 inputNumbers()와 입력받은 번호의 형식이 올바른지 체크하는 inputNumbersValidate()
    inputNumbers()에서 당첨 번호를 입력하고 inputNumbersValidate()에 넣어서 형식이 올바른지 확인한다. 올바르다면 입력한 String을 return해주고, 올바르지 않다면 예외처리를 해준다. 단위테스트를 통해 예외처리됨을 확인하였다.

--- src/main/java/lotto/DrawLotto.java
+++ src/main/java/lotto/DrawLotto.java
@@ -14,18 +14,47 @@ public class DrawLotto {
 
     public void drawingLotto() {
         User user = new User();
+        System.out.println("구입금액을 입력해 주세요.");
         user.setMoneyAndLottoCount(inputMoney()); //구입 금액을 입력받고 1000원 단위가 맞다며 해당 금액과 금액/1000 한 값을 user객체의 setter함수에 전달한다.
-        user.setUserLottos(getLottoTickets(user.getLottoCount())); //사용자가 가진 구입가능 티켓 숫자를 넘겨부면 getLottoTickets 함수에서 그 수만큼 로또를 발행해준다.
+        user.getLottoTickets(); //사용자가 가진 구입가능 티켓 숫자만큼 getLottoTickets 함수에서 그 수만큼 로또를 발행하고, 그 결과를 출력해준다.
+
     }
 
     public int inputMoney() {
         return Integer.parseInt(Console.readLine());
     }
 
-    public void inputValidate(int money) {
+    public void inputMoneyValidate(int money) {
         if (money != Constants.LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력 받습니다.");
         }
+    }
+
+    public String inputNumbers(){
+        String input=Console.readLine();
+        inputNumbersValidate(input);
+
+        return input;
+    }
+
+    public void inputNumbersValidate(String input){
+        //","가 포함되지 않은 입력이면 예외처리
+        if(!input.contains(",")){
+            throw new IllegalArgumentException("[ERROR] 입력된 형식이 올바르지 않습니다(번호를 쉼표(,)를 기준으로 구분하여 입력하세요).");
+        }
+
+        String[] inputs=input.split(",");
+        //쉼표(,)로 구분된 문자열이 하나라도 숫자가 아니라면 예외처리
+        try {
+            for(String str : inputs){
+                int num=Integer.parseInt(str);
+            }
+        }
+        catch (NumberFormatException e){
+            System.out.println("[ERROR] 입력된 형식이 올바르지 않습니다(숫자를 입력하세요).");
+            throw e;
+        }
+
     }
 
 }
--- src/test/java/lotto/DrawLottoTest.java
+++ src/test/java/lotto/DrawLottoTest.java
@@ -13,10 +13,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class DrawLottoTest {
     @DisplayName("입력받은 구입 금액이 1000원 단위가 아니면 예외를 발생시킵니다.")
     @Test
-    void inputValidate(){
+    void inputMoneyValidate(){
         DrawLotto drawLotto= new DrawLotto();
 
-        assertThatThrownBy(() -> drawLotto.inputValidate(9500))
+        assertThatThrownBy(() -> drawLotto.inputMoneyValidate(9500))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("입력받은 당첨 번호에 쉼표(,)가 없다면 예외를 발생시킵니다.")
+    @Test
+    void inputNumbersValidate1(){
+        DrawLotto drawLotto= new DrawLotto();
+
+        assertThatThrownBy(() -> drawLotto.inputNumbersValidate("123456"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("입력받은 당첨 번호에 숫자가 아닌 문자가 들어있으면 예외를 발생시킵니다.")
+    @Test
+    void inputNumbersValidate2(){
+        DrawLotto drawLotto= new DrawLotto();
+
+        assertThatThrownBy(() -> drawLotto.inputNumbersValidate("1,2,3,4,5,ㅇ"))
+                .isInstanceOf(NumberFormatException.class);
+    }
+
+
 }
