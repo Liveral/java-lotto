7f1f8fa6beef67a3a4daef1ebdab308aea18aca9
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-02 16:22:46 +0900

    feat : 구입 가능한 로또 개수만큼 로또를 발행해 주는 getLottoTickets()
    구입 가능한 로또 개수를 매개변수로 받은 후 해당 수만큼 랜덤한 숫자 6자리로 구성된 리스트를 만들고 만들어진 2차원 리스트를 return 하는 함수를 만들었다.

--- src/main/java/lotto/DrawLotto.java
+++ src/main/java/lotto/DrawLotto.java
@@ -1,5 +1,10 @@
 package lotto;
+
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DrawLotto {
 
@@ -7,20 +12,33 @@ public class DrawLotto {
     }
 
 
-    public void drawingLotto(){
-        User user=new User();
-        user.setMoneyAndLottoCount(inputMoney());
-
+    public void drawingLotto() {
+        User user = new User();
+        user.setMoneyAndLottoCount(inputMoney()); //구입 금액을 입력받고 1000원 단위가 맞다며 해당 금액과 금액/1000 한 값을 user객체의 setter함수에 전달한다.
+        user.setUserLottos(getLottoTickets(user.getLottoCount())); //사용자가 가진 구입가능 티켓 숫자를 넘겨부면 getLottoTickets 함수에서 그 수만큼 로또를 발행해준다.
     }
-    public int inputMoney(){
+
+    public int inputMoney() {
         return Integer.parseInt(Console.readLine());
     }
 
-    public void inputValidate(int money){
-        if(money!=Constants.LOTTO_NUMBER_SIZE){
+    public void inputValidate(int money) {
+        if (money != Constants.LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력 받습니다.");
         }
     }
+
+    public List<List<Integer>> getLottoTickets(int lottoCount) {
+        List<List<Integer>> lottoTickets=new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottoTickets.add(numbers);
+        }
+        return lottoTickets;
+    }
+
+
 
 }
 
