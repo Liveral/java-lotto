bdfc1cca9e7ffc8966a77d58c987cd847e190432
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-02 18:45:36 +0900

    feat : 입력받은 당첨 번호에 중복을 체크하는 duplicateCheck()
    입력받은 List를 HashSet<>()은 중복저장을 허용하지 않기 때문에 만약 입력받은 당첨 번호에 중복된 숫자가 있었다면 Set의 사이즈가 기존 List의 사이즈보다 작아졌을 것입니다. 그래서 길이가 다르다면 예외처리를 해줍니다. 단위테스트를 통해 예외처리됨을 확인하였습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,34 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        sizeCheck(numbers);
+        duplicateCheck(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void sizeCheck(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void duplicateCheck(List<Integer> numbers){
+        Set<Integer> OneandOnlyNumbers = new HashSet<>(numbers);    //HashSet<>()은 중복저장을 허용하지 않습니다.
+
+        if (OneandOnlyNumbers.size() != Constants.LOTTO_NUMBER_SIZE) {  //Set의 길이가 LOTTO_NUMBER_SIZE와 다르다면 중복되는 숫자가 있어서 변했다는 뜻
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+        }
+
+    }
+
+
+
+
 }
