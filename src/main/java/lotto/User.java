688f3d8ffba9a12d71343f9842ce30f3477861de
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-03 16:04:15 +0900

    feat : 사용자의 당첨금과 당첨금을 저장하는 getWinnings() 구현
    로또에 당첨될때마다 그 금액을 저장해놓을 winnings 변수와 저장하는 함수 getWinnings를 추가하였다.

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -9,7 +9,12 @@ import java.util.List;
 public class User {
     private int money; //사용자의 구입 금액
     private int lottoCount; // 사용자가 구매한 로또의 개수
-    private List<List<Integer>> userLottos = new ArrayList<>(); //사용자가 가지고 있는 로또의 리스트
+    private final List<List<Integer>> userLottos = new ArrayList<>(); //사용자가 가지고 있는 로또의 리스트
+    private int winnings=0; //사용자의 당첨금
+
+    public void getWinnings(int money) {
+        winnings+=money;
+    }
 
     public User() {
     }
@@ -17,7 +22,9 @@ public class User {
     public int getMoney() {
         return money;
     }
-
+    public List<Integer> getUserLottos(int index){
+        return userLottos.get(index); //index번째 로또를 return 해준다.
+    }
     public void setMoneyAndLottoCount(int money) {
         this.money = money;
         this.lottoCount = money / 1000;
@@ -29,11 +36,8 @@ public class User {
         return lottoCount;
     }
 
-    public void setUserLottos(List<List<Integer>> lottoTicket) {
-        this.userLottos = lottoTicket;
-    }
 
-    public void getLottoTickets() {
+    public void setLottoTickets() {
 
         for (int i = 0; i < lottoCount; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
