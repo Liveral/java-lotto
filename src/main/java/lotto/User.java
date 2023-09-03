001e947d9bb1e8e8abfb9ce6c22db32909278636
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-03 18:53:54 +0900

    feat : 로또 결과를 출력하는 printLottoResult() 구현
    결과를 어떻게 출력해야 할지 잘 모르겠어서 약간 하드코딩식으로 구현하였다. 숫자의 쉼표 표현은 DecimalFormat 클래스의 format함수를 이용하였다.

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,10 +11,15 @@ public class User {
     private int money; //사용자의 구입 금액
     private int lottoCount; // 사용자가 구매한 로또의 개수
     private final List<List<Integer>> userLottos = new ArrayList<>(); //사용자가 가지고 있는 로또의 리스트
-    private int winnings=0; //사용자의 당첨금
+    private int[] ranks=new int[Constants.LOTTO_NUMBER_SIZE]; // 사용자가 각 등수를 몇번 했는지 기록할 배열.
+    private long winnings=0; //사용자의 당첨금
 
-    public void getWinnings(int money) {
+
+    public void setWinnings(long money) {
         winnings+=money;
+    }
+    public void setRanks(int rank){
+        ranks[rank]++;
     }
 
     public User() {
@@ -51,5 +57,13 @@ public class User {
         for (int i = 0; i < lottoCount; i++) {
             System.out.println(userLottos.get(i).toString());
         }
+    }
+    public void printLottoResult(){
+        DecimalFormat df = new DecimalFormat("###,###");
+        System.out.printf("3개 일치 (%s원) - %d개\n",df.format(Result.getReward(3)),ranks[5]);
+        System.out.printf("4개 일치 (%s원) - %d개\n",df.format(Result.getReward(4)),ranks[4]);
+        System.out.printf("5개 일치 (%s원) - %d개\n",df.format(Result.getReward(5)),ranks[3]);
+        System.out.printf("5개 일치, 보너스 볼 일치 (%s원) - %d개\n",df.format(Result.getReward(10)),ranks[2]);
+        System.out.printf("6개 일치 (%s원) - %d개\n",df.format(Result.getReward(6)),ranks[1]);
     }
 }
