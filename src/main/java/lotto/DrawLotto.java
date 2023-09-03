f7258343efbe8e55dd898bccd93dd1c51952773e
Yeonwoo <yoanwoo0227@gmail.com>
2023-09-03 16:06:37 +0900

    feat : 당첨을 확인할 checkWinning()함수 구현
    당첨 번호와 비교를 하기 위해 사용자가 가진 로또 개수만큼 compareNumbers()를 실행하도록 구현하였다.

--- src/main/java/lotto/DrawLotto.java
+++ src/main/java/lotto/DrawLotto.java
@@ -1,10 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DrawLotto {
@@ -14,18 +12,17 @@ public class DrawLotto {
     public DrawLotto() {
     }
 
-
     public void drawingLotto() {
         user = new User();
 
         System.out.println("구입금액을 입력해 주세요.");
         user.setMoneyAndLottoCount(inputMoney()); //구입 금액을 입력하면 user객체에 구입 금액과 구매할 로또의 개수가 setting된다.
-        user.getLottoTickets(); //사용자가 가진 구입가능 티켓 숫자만큼 getLottoTickets 함수에서 로또를 발행 및 번호 출력.
+        user.setLottoTickets(); //사용자가 가진 구입가능 티켓 숫자만큼 setLottoTickets 함수에서 로또를 발행 및 번호 출력.
         System.out.println("당첨 번호를 입력해 주세요.");
         lotto = new Lotto(inputNumbers());
         System.out.println("보너스 번호를 입력해 주세요.");
         lotto.setBonusNumber(inputBonusNumber());
-
+        checkWinning(); // 당첨 확인
 
     }
 
@@ -50,16 +47,20 @@ public class DrawLotto {
 
         return numbers;
     }
+
     public static int inputBonusNumber() {
         String bonusNumber = Console.readLine();
+        ExceptionHandling.isInputNumbers(bonusNumber);
 
-        ExceptionHandling.isInputNumbers(bonusNumber); //입력한 보너스 번호가 숫자형식인지 확인
         return Integer.parseInt(bonusNumber);
     }
 
-    public void playDrawing() {
-
+    public void checkWinning() {
+        for (int i=0;i<user.getLottoCount();i++){
+            compareNumbers(lotto.getNumbers(),user.getUserLottos(i)); //당첨 번호와 사용자의 i번째 로또를 비교한다.
+        }
     }
+
 
 }
 
